1d484c0392ccd3ba9e625ebfc81baae021a9cf78
Thomas Griffiths <thomasgriffiths11@hotmail.com>
2016-12-03 18:59:58 -0700

    Changed back to html

---
+++ scripts/myAngular.js
@@ -0,0 +1,9 @@
+//custom javascript
+
+var graphCalc = angular.module("theModule",[]);
+
+var controller1 = function($scope) {
+  $scope.test = "This is a test";
+};
+
+graphcalc.controller1("controller1", controller1);

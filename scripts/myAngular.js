76e409f7def3b44d8bbf91cdba60a541d3105f9d
Thomas Griffiths <thomasgriffiths11@hotmail.com>
2016-12-03 23:41:31 -0700

    the days work

--- scripts/myAngular.js
+++ scripts/myAngular.js
@@ -1,9 +1,35 @@
 //custom javascript
 
-var graphCalc = angular.module("theModule",[]);
+var graphCalc = angular.module("theModule",['ngSanitize']);
 
 var controller1 = function($scope) {
-  $scope.test = "This is a test";
+	/*
+  var operators = [
+    {forms: [{regex: /e\^/, string: "e^"}], want: "exp()", check: /exp\(*\)/, changeThis: "exp", changeTo: "Math.exp"},
+    {changeThis: "sqrt", changeTo: "Math.sqrt"}
+  ];
+  */
+  var operators = ["+","-","/","*","%","^"];
+  $scope.userInput = "";
+  $scope.evaluate = function(input){
+  	//if(input.match(/[^A-Za-z0-9\*\/\+\-_]/)) return "Not a valid expression.";
+  	if(!input) return "Enter an expression";
+  	if(input.match(/[^0-9A-Za-z+-/*^%()]/)) return "Not a valid expression";
+	var openPar = input.match(/\(/)||[];
+	var closePar = input.match(/\)/)||[];
+    if(openPar.length != closePar.length) return "Check Paranetheses";
+	var matches = input.match(/[+-/*^%]/)||[];
+	var valid = input.match(/[0-9xe)][+-/*^%][0-9xe(]/)||[];
+	if(matches.length > valid.length) return "Incomplete Statement";
+	var result = "";
+	for(i=0; i<10; i++) {
+	  var x = i;
+	  var evaluate = input.replace("x",x);
+	  result += math.eval(evaluate) + "<br>";
+	}
+
+    return result;
+  }
 };
 
-graphcalc.controller1("controller1", controller1);
+graphCalc.controller("controller1", controller1);
